e1082eeadf840a91834ddf6d3aabd0ac4b6c671d
BlastTea <arya.jbr.999@gmail.com>
2024-06-17 01:34:25 +0700

    feat: change every path to call path fucntion

--- resources/views/auth/register.php
+++ resources/views/auth/register.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <?php include __DIR__ . '\..\root_components\head_app.php'; ?>
+    <?php include resource_path('root_components\head_app.php'); ?>
     <title>Sign In | TailAdmin - Tailwind CSS Admin Dashboard Template</title>
 </head>
 
@@ -14,7 +14,7 @@
     :class="{'dark text-bodydark bg-boxdark-2': darkMode === true}">
 
     <!-- ===== Preloader Start ===== -->
-    <?php include __DIR__ . '\..\root_components/loading.php' ?>
+    <?php include resource_path('root_components/loading.php') ?>
     <!-- ===== Preloader End ===== -->
 
     <!-- ===== Content Area Start ===== -->
